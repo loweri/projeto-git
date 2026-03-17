fddcc9b2121c646726825e52fda77c05dde559dd
Erior <ericlesg@yahoo.com.br>
2026-03-16 23:13:24 -0300

    Daily studies

--- Projetos Pequenos/SistemaDeCadastro(PYpuroDBZ)_v2.py
+++ Projetos Pequenos/SistemaDeCadastro(PYpuroDBZ)_v2.py
@@ -30,6 +30,41 @@ def carregar():
             temp_skill = per["habilidades"]
             rebuilt = Personagem_DB(temp_name, temp_power, temp_skill)
             personagens_DB.append(rebuilt)
+# ================= função para atualizar personagens cadastrados ================= #
+def atualizar():
+    print("Buscando lista de personagens...")
+    if not personagens_DB:
+        print("Ainda não há personagens registrados. Digite novamente uma opção válida do menu.")
+        return
+    for p in personagens_DB:
+        print(p)
+    print("Qual personagem gostaria de atualizar?")
+    atualizar_p = input().strip().lower()
+    for personagem in personagens_DB:
+        if atualizar_p == personagem.nome:
+            print(f"Personagem {atualizar_p} foi encontrado, escolha qual atributo deseja alterar:")
+            print("Digite o número correspondente: 1 - Nome; 2 - Poder; 3 - Habilidades:")
+            pe_escolha = int(input().strip())
+            if pe_escolha == 1:
+                print(f"Opção 'Alterar nome' escolhida para {atualizar_p}, forneça a correção:")
+                pe1_corrigir = input().strip().lower()
+                personagem.nome = pe1_corrigir
+                print(f"O nome com alteração agora é: {pe1_corrigir}.")
+            elif pe_escolha == 2:
+                print(f"Opção 'Alterar Poder' escolhida para {atualizar_p}, forneça a correção (apenas numeros inteiros, sem virgula ou ponto):")
+                pe2_corrigir = int(input().strip())
+                personagem.poder = pe2_corrigir
+            elif pe_escolha == 3:
+                print(f"Opção 'Alterar Habilidade', forneça a correção:")
+                print("Habilidade 1 - correção:")
+                pe3_1_corrigir = input().strip().lower()
+                print("Habilidade 2 - correção:")
+                pe3_2_corrigir = input().strip().lower()
+                personagem.habilidades = [pe3_1_corrigir,pe3_2_corrigir]
+            salvar()
+            break
+    else:
+        print("Parece que a busca não encontrou, verifique a ortografia e tente novamente!")
 # ================= função para funcionar o sistema de cadastro ================= #
 def cadastro():
     print("Por favor, digite o nome do personagem: ")
@@ -60,8 +95,9 @@ print("Bem vindo guerreiros e guerreiras, por favor, escolha uma das opções ab
 print(f"01 - Cadastrar novo personagem!" \
 "\n02 - Lista de personagens." \
 "\n03 - Buscar por nome." \
-"\n04 - Sair do programa.")
-print("Digite apenas o numero individual (1, 2, 3 ou 4): ")
+"\n04 - Atualizar informação de personagem." \
+"\n05 - Sair do programa.")
+print("Digite apenas o numero individual (1, 2, 3, 4 ou 5): ")
 while True:
     opcao_selecionada = int(input())
     if opcao_selecionada == 1:
@@ -78,6 +114,12 @@ while True:
             if buscarnome == personagens.nome:
                 print(personagens)
                 break
+        else:
+            print("Erro, personagem não encontrado. Verifique a ortografia e tente novamente!")
     elif opcao_selecionada == 4:
+        print("Atualizar informações:")
+        atualizar()
+    elif opcao_selecionada == 5:
         print("Opção ""Sair"" selecionada, obrigado por utilizar o programa, até a próxima!")
         break
+# ================= fim da v2 - em breve v3 com mais adições: deletar personagem, validar input e tratar erro, função de comparação entre dois personagens ================= #
